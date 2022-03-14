790b6e56a774fb6dc4540d59d1706c0d78a86cbc
edikgoose <edikgoose@gmail.com>
2022-03-14 20:05:23 +0300

    Modify solution class, add own exceptions

--- src/eduard/zaripov/Main.java
+++ src/eduard/zaripov/Main.java
@@ -1,6 +1,41 @@
 package eduard.zaripov;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.*;
+
+class IllegalInputCoordinate extends Exception {
+    TypeOfNode first;
+    TypeOfNode second;
+
+    public IllegalInputCoordinate(TypeOfNode fist, TypeOfNode second) {
+        super();
+        this.first = fist;
+        this.second = second;
+    }
+
+    @Override
+    public String getMessage() {
+        return TypeOfNode.toString(first) + " cannot be at the same coordinate as " + TypeOfNode.toString(second);
+    }
+}
+
+class HarryIsCapturedException extends Exception {
+    private final ArrayList<Coordinate> capturedPath;
+
+    public HarryIsCapturedException(ArrayList<Coordinate> capturedPath) {
+        this.capturedPath = capturedPath;
+    }
+
+    public ArrayList<Coordinate> getCapturedPath() {
+        return capturedPath;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Harry is captured";
+    }
+}
 
 class Coordinate {
     private final int X;
@@ -62,11 +97,6 @@ class IO {
     static Integer readInputMode() {
         System.out.println("Input mode:\n 1: Keyboard\n 2: Random");
         return Integer.parseInt(scanner.nextLine());
-    }
-
-
-    static void viewGameOverMessage() {
-        System.out.println("The Harry is captured by a guard! Game over.");
     }
 
     static void newLine() {
@@ -224,7 +254,7 @@ class Grid {
                 Coordinate catPosition,
                 Coordinate bookPosition,
                 Coordinate cloakPosition,
-                Coordinate exitPosition) {
+                Coordinate exitPosition) throws IllegalInputCoordinate, HarryIsCapturedException {
         grid = new Node[sizeOfGrid][sizeOfGrid];
 
         for (int row = 0; row < grid.length; row++) {
@@ -240,7 +270,7 @@ class Grid {
             grid[exitPosition.getX()][exitPosition.getY()].addTypeOfNode(TypeOfNode.EXIT);
         }
         else {
-            throw new IllegalArgumentException("Illegal map configuration");
+            throw new IllegalInputCoordinate(TypeOfNode.BOOK, TypeOfNode.EXIT);
         }
 
         addInspector(filthPosition, radiusOfStrongInspector);
@@ -255,20 +285,20 @@ class Grid {
         }
 
         if (!isSafe(startPosition, false)) {
-            IO.viewGameOverMessage();
-            IO.printString(toString());
-            System.exit(0);
+            ArrayList<Coordinate> capturedPath = new ArrayList<>();
+            capturedPath.add(startPosition);
+            throw new HarryIsCapturedException(capturedPath);
         }
     }
 
-    public void addInspector(Coordinate coordinate, int radius) {
+    public void addInspector(Coordinate coordinate, int radius) throws IllegalInputCoordinate {
         for (int row = coordinate.getX() - radius; row <= coordinate.getX() + radius; row++) {
             for (int column = coordinate.getY() - radius; column <= coordinate.getY() + radius; column++) {
                 if(row < 0 || row >= grid.length || column < 0 || column >= grid.length){
                     continue;
                 }
                 if (grid[row][column].containsCrucialElements()) {
-                    throw new IllegalArgumentException("Illegal map configuration");
+                    throw new IllegalInputCoordinate(getNode(coordinate).getTypesOfNode().first(), TypeOfNode.DANGER);
                 }
                 if (!grid[row][column].getTypesOfNode().contains(TypeOfNode.INSPECTOR)) {
                     grid[row][column].addTypeOfNode(TypeOfNode.DANGER);
@@ -276,7 +306,7 @@ class Grid {
             }
         }
         if(getNode(coordinate).containsCrucialElements()) {
-            throw new IllegalArgumentException("Illegal map configuration");
+            throw new IllegalInputCoordinate(getNode(coordinate).getTypesOfNode().first(), TypeOfNode.INSPECTOR);
         }
         grid[coordinate.getX()][coordinate.getY()].getTypesOfNode().remove(TypeOfNode.DANGER);
         grid[coordinate.getX()][coordinate.getY()].addTypeOfNode(TypeOfNode.INSPECTOR);
@@ -293,7 +323,7 @@ class Grid {
                 (!grid[coordinate.getX()][coordinate.getY()].isDangerOrInspector());
     }
 
-    public ArrayList<Coordinate> findPathBacktracking(Coordinate startPosition, Coordinate endPosition, boolean isInvisible, int mode) {
+    public ArrayList<Coordinate> findPathBacktracking(Coordinate startPosition, Coordinate endPosition, boolean isInvisible, int mode) throws HarryIsCapturedException {
         clearPaths();
         if(!findPathBacktrackingRecursive(startPosition, endPosition, isInvisible, mode)) {
             return null;
@@ -301,7 +331,7 @@ class Grid {
         return restorePath(endPosition);
     }
 
-    private boolean findPathBacktrackingRecursive(Coordinate startPosition, Coordinate endPosition, boolean isInvisible, int mode) {
+    private boolean findPathBacktrackingRecursive(Coordinate startPosition, Coordinate endPosition, boolean isInvisible, int mode) throws HarryIsCapturedException {
         if (startPosition.equals(endPosition) && isSafe(startPosition, isInvisible)) {
             grid[startPosition.getX()][startPosition.getY()].setIsPath(true);
             return true;
@@ -334,11 +364,7 @@ class Grid {
         }
 
         if(mode == 2 && !grid[startPosition.getX()][startPosition.getY()].isDetectedAsDanger()) {
-            IO.viewGameOverMessage();
-
-            toString(restorePath(startPosition));
-
-            System.exit(0);
+            throw new HarryIsCapturedException(restorePath(startPosition));
         }
 
         return false;
@@ -431,7 +457,7 @@ class Grid {
         return Math.sqrt(Math.pow(startPosition.getX() - endPosition.getX(), 2) + Math.pow(startPosition.getY() - endPosition.getY(),2));
     }
 
-    public ArrayList<Coordinate> findPathAStar(Coordinate startPosition, Coordinate endPosition, boolean isInvisible, int mode) {
+    public ArrayList<Coordinate> findPathAStar(Coordinate startPosition, Coordinate endPosition, boolean isInvisible, int mode) throws HarryIsCapturedException {
         clearPaths();
         LinkedList<Coordinate> open = new LinkedList<>();
         ArrayList<Coordinate> closed = new ArrayList<>();
@@ -481,11 +507,7 @@ class Grid {
             }
             else {
                 if (mode == 2 && !getNode(current).isDetectedAsDanger()) {
-                    IO.viewGameOverMessage();
-
-                    IO.printString(toString(restorePath(current)));
-
-                    System.exit(0);
+                    throw new HarryIsCapturedException(restorePath(current));
                 }
             }
         }
@@ -576,33 +598,61 @@ class Grid {
     }
 }
 
-class RandomInput {
-    static int upperBound = Solution.sizeOfGrid;
-    private static final Random random = new Random();
+class Solution {
+    static int sizeOfGrid = 6;
+    static int filchRadius = 1;
+    static int catRadius = 0;
 
-    public static int getRandomInputMode() {
-        return random.nextInt(2) + 1;
-    }
+    static class RandomInput {
+        private static final int upperBound = Solution.sizeOfGrid;
+        private static final Random random = new Random();
 
-    public static ArrayList<Coordinate> getRandomInputCoordinates(){
-        ArrayList<Coordinate> coordinates = new ArrayList<>();
-
-        for (int i = 0; i < 6; i++) {
-            coordinates.add(getRandomCoordinates());
+        public static int getRandomInputMode() {
+            return random.nextInt(2) + 1;
         }
 
-        return coordinates;
-    }
+        public static ArrayList<Coordinate> getRandomInputCoordinates(){
+            ArrayList<Coordinate> coordinates = new ArrayList<>();
+            for (int i = 0; i < 3; i++) {
+                coordinates.add(getRandomCoordinates());
+            }
+            Coordinate filchCoordinate = coordinates.get(1);
+            Coordinate catCoordinate = coordinates.get(2);
 
-    private static Coordinate getRandomCoordinates() {
-        return new Coordinate(random.nextInt(upperBound), random.nextInt(upperBound));
-    }
-}
+            for (int i = 0; i < 3; i++) {
+                Coordinate currentCoordinate = getRandomCoordinates();
+                while (isInDanger(currentCoordinate, filchCoordinate, filchRadius) ||
+                        isInDanger(currentCoordinate, catCoordinate, catRadius)) {
+                    currentCoordinate = getRandomCoordinates();
+                }
+                coordinates.add(currentCoordinate);
+            }
 
-class Solution {
-    static int sizeOfGrid = 9;
-    static int filchRadius = 2;
-    static int catRadius = 1;
+            while (coordinates.get(5).equals(coordinates.get(3)) ||
+                    isInDanger(coordinates.get(5), filchCoordinate, filchRadius) ||
+                    isInDanger(coordinates.get(5), catCoordinate, catRadius)) {
+                coordinates.set(5, getRandomCoordinates());
+            }
+
+            return coordinates;
+        }
+
+        private static Coordinate getRandomCoordinates() {
+            return new Coordinate(random.nextInt(upperBound), random.nextInt(upperBound));
+        }
+
+        private static  boolean isInDanger(Coordinate currentCoordinate, Coordinate inspectorCoordinate, int radius) {
+            for (int i = inspectorCoordinate.getX() - radius; i <= inspectorCoordinate.getX() + radius; i++) {
+                for (int j = inspectorCoordinate.getY() - filchRadius; j <= inspectorCoordinate.getY() + filchRadius; j++) {
+                    if (currentCoordinate.equals(new Coordinate(i, j))) {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
 
     enum TypeOfSearch {
         ASTAR, BACKTRACKING;
@@ -614,43 +664,82 @@ class Solution {
     Coordinate cloakPosition;
     Coordinate bookPosition;
     Coordinate exitPosition;
+    HashMap<ArrayList<Coordinate>, Boolean> allScenarios;
+    ArrayList<Coordinate> mostProfitScenario;
 
-    public Solution(ArrayList<Coordinate> inputCoordinates, int mode) {
+    public Solution(ArrayList<Coordinate> inputCoordinates, int mode) throws IllegalArgumentException {
         this.harryPosition = inputCoordinates.get(0);
         this.bookPosition = inputCoordinates.get(3);
         this.cloakPosition = inputCoordinates.get(4);
         this.exitPosition = inputCoordinates.get(5);
-        this.grid = new Grid(sizeOfGrid, filchRadius, catRadius, harryPosition, inputCoordinates.get(1), inputCoordinates.get(2), bookPosition, cloakPosition, exitPosition);
+
+        try {
+            this.grid = new Grid(sizeOfGrid, filchRadius, catRadius, harryPosition, inputCoordinates.get(1), inputCoordinates.get(2), bookPosition, cloakPosition, exitPosition);
+        }
+        catch (IllegalInputCoordinate e) {
+            IO.printString(e.getMessage());
+            System.exit(1);
+        }
+        catch (HarryIsCapturedException e) {
+            IO.printString(e.getMessage());
+            System.exit(1);
+        }
+        IO.printString("Initial grid: ");
+        IO.printString(grid.toString());
+
         this.mode = mode;
+        this.allScenarios = new HashMap<>();
+        ArrayList<ArrayList<Coordinate>> scenarios = getAllPossibleScenarios(harryPosition, cloakPosition, bookPosition, exitPosition);
+        for (ArrayList<Coordinate> scenario : scenarios) {
+            allScenarios.put(scenario, true);
+        }
+    }
+
+    public Solution() {
+        this(RandomInput.getRandomInputCoordinates(), RandomInput.getRandomInputMode());
     }
 
     public ArrayList<ArrayList<Coordinate>> findPath(TypeOfSearch typeOfSearch) {
-        ArrayList<ArrayList<Coordinate>> allScenarios = getAllPossibleScenarios(harryPosition, cloakPosition, bookPosition, exitPosition);
-
         ArrayList<ArrayList<Coordinate>> minPath = null;
         int minLength = Integer.MAX_VALUE;
-        for(ArrayList<Coordinate> scenario : allScenarios) {
-            ArrayList<ArrayList<Coordinate>> path = calculatePath(typeOfSearch, grid, mode, scenario);
-            if (path != null) {
-                int overallLength = 0;
-                for (ArrayList<Coordinate> currentPath : path) {
-                    overallLength += currentPath.size();
-                }
+        for(ArrayList<Coordinate> scenario : allScenarios.keySet()) {
+//            if (!allScenarios.get(scenario)) {
+//                continue;
+//            }
+            try {
+//                if (typeOfSearch == TypeOfSearch.BACKTRACKING && mostProfitScenario != null) {
+//                    return calculatePath(typeOfSearch, grid, mode, mostProfitScenario);
+//                }
 
-                if (minPath == null) {
-                    minPath = path;
-                    minLength = overallLength;
+                ArrayList<ArrayList<Coordinate>> path = calculatePath(typeOfSearch, grid, mode, scenario);
+                if (path != null) {
+                    int overallLength = 0;
+                    for (ArrayList<Coordinate> currentPath : path) {
+                        overallLength += currentPath.size();
+                    }
+                    if (minPath == null) {
+                        mostProfitScenario = scenario;
+                        minPath = path;
+                        minLength = overallLength;
+                    } else if (overallLength < minLength) {
+                        mostProfitScenario = scenario;
+                        minPath = path;
+                        minLength = overallLength;
+                    }
+                } else {
+                    allScenarios.replace(scenario, false);
                 }
-
-                if (overallLength < minLength) {
-                    minPath = path;
-                }
+            }
+            catch (HarryIsCapturedException e) {
+                IO.printString(e.getMessage());
+                IO.printString(grid.toString(e.getCapturedPath()));
+                System.exit(1);
             }
         }
         return minPath;
     }
 
-    private ArrayList<ArrayList<Coordinate>> calculatePath(TypeOfSearch typeOfSearch, Grid grid, int mode, ArrayList<Coordinate> scenario) {
+    private ArrayList<ArrayList<Coordinate>> calculatePath(TypeOfSearch typeOfSearch, Grid grid, int mode, ArrayList<Coordinate> scenario) throws HarryIsCapturedException {
         boolean isCloakInPath = false;
         ArrayList<ArrayList<Coordinate>> path = new ArrayList<>();
 
@@ -717,129 +806,30 @@ class Solution {
 
 public class Main {
     public static void main(String[] args) {
-        //int inputMode = IO.readInputMode();
-
-        ArrayList<Coordinate> coordinates = IO.readCoordinates();
-        int mode = IO.readMode();
-
-        Solution solution = new Solution(coordinates, mode);
-
-        ArrayList<ArrayList<Coordinate>> path1 = solution.findPath(Solution.TypeOfSearch.BACKTRACKING);
-
-        IO.printString(solution.toString(path1));
-
-        /*
-        int inputMode;
-
-        if(args.length > 0) {
-            inputMode = 2;
+        int inputMode = IO.readInputMode();
+        Solution solution;
+        if (inputMode == 1) {
+             solution = new Solution(IO.readCoordinates(), IO.readMode());
         }
         else {
-            inputMode = IO.readInputMode();
+            solution = new Solution();
         }
 
-        ArrayList<Coordinate> coordinates = new ArrayList<>();
-        int mode = 0;
-        try {
-            switch (inputMode) {
-                case 1:
-                    coordinates = IO.readCoordinates();
-                    mode = IO.readMode();
-                    break;
-                case 2:
-                    coordinates = RandomCoordinates.getRandomInputCoordinates();
-                    mode = RandomCoordinates.getRandomMode();
-                    break;
-                default:
-                    IO.viewErrorInputMessage();
-            }
-        }
-        catch (IllegalArgumentException e) {
-            IO.printString(e.getMessage());
-            main(null);
-        }
+        long start0 = System.currentTimeMillis();
+        ArrayList<ArrayList<Coordinate>> path1 = solution.findPath(Solution.TypeOfSearch.ASTAR);
+        long end0 = System.currentTimeMillis();
+        IO.printString("Elapsed Time in milli seconds: "+ (end0 - start0));
+        IO.printString(solution.toString(path1));
 
-        try {
-            Grid grid = new Grid(sizeOfGrid, filchRadius, catRadius, coordinates.get(0), coordinates.get(1), coordinates.get(2), coordinates.get(3), coordinates.get(4), coordinates.get(5));
+        IO.newLine();
 
-            for (Coordinate coordinate : coordinates) {
-                System.out.print("["+coordinate.getX()+","+coordinate.getY()+"] ");
-            }
-            IO.newLine();
-            IO.printString("Mode: " + mode);
+        long start1 = System.currentTimeMillis();
+        ArrayList<ArrayList<Coordinate>> path2 = solution.findPath(Solution.TypeOfSearch.BACKTRACKING);
+        long end1 = System.currentTimeMillis();
+        IO.printString("Elapsed Time in milli seconds: "+ (end1-start1));
+        IO.printString(solution.toString(path2));
 
-            IO.printString("Initial grid: ");
-            IO.printString(grid.toString());
-
-            IO.newLine();
-
-            ArrayList<Coordinate> pathHarryToCloak = grid.findPathAStar(coordinates.get(0), coordinates.get(4), false, mode);
-            if (pathHarryToCloak == null) {
-                IO.printString("No path");
-            }
-            else {
-                IO.printString(grid.toString(pathHarryToCloak));
-            }
-
-            IO.newLine();
-
-            ArrayList<Coordinate> pathCloakToBook = grid.findPathAStar(coordinates.get(4), coordinates.get(3), true, mode);
-            if (pathCloakToBook == null) {
-                IO.printString("No path");
-            }
-            else {
-                IO.printString(grid.toString(pathCloakToBook));
-            }
-
-            IO.newLine();
-
-            ArrayList<Coordinate> pathBookToExit = grid.findPathAStar(coordinates.get(3), coordinates.get(5), true, mode);
-            if (pathBookToExit == null) {
-                IO.printString("No path");
-            }
-            else {
-                IO.printString(grid.toString(pathBookToExit));
-            }
-
-            IO.newLine();
-            IO.printString("BACKTRACKING!");
-            IO.newLine();
-
-            ArrayList<Coordinate> pathHarryToCloakB = grid.findPathBacktracking(coordinates.get(0), coordinates.get(4), false, mode);
-            if (pathHarryToCloakB == null) {
-                IO.printString("No path");
-            }
-            else {
-                IO.printString(grid.toString(pathHarryToCloakB));
-            }
-
-            IO.newLine();
-
-            ArrayList<Coordinate> pathCloakToBookB = grid.findPathBacktracking(coordinates.get(4), coordinates.get(3), true, mode);
-            if (pathCloakToBookB == null) {
-                IO.printString("No path");
-            }
-            else {
-                IO.printString(grid.toString(pathCloakToBookB));
-            }
-
-            IO.newLine();
-
-            ArrayList<Coordinate> pathBookToExitB = grid.findPathBacktracking(coordinates.get(3), coordinates.get(5), true, mode);
-            if (pathBookToExitB == null) {
-                IO.printString("No path");
-            }
-            else {
-                IO.printString(grid.toString(pathBookToExitB));
-            }
-
-        }
-        catch (IllegalArgumentException e) {
-            String[] arr = new String[1];
-            arr[0] = "2";
-            main(arr);
-
-        }
-         */
+        IO.newLine();
+        //IO.printString(solution.toString(path2));
     }
 }
